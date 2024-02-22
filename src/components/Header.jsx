56b9e6e718ac7e1875249eb5217578819f8f0a80
hardimahendra <hardimahendra35@gmail.com>
2024-02-22 19:59:14 +0700

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
         <div className="flex flex-column justify-center items-center text-center w-full h-full">
           <h1 className="text-4xl text-orange-500 font-serif">Anak Jadi Suka Dan Jago Matematika Dalam Sekejap. Cuma Di Les Matematika Online Bimbel XYZ !</h1>
           <p className="text-2xl text-white font-serif">Les Matematika Online dengan metode belajar interaktif, tutor Profesional dan investasi belajar yang terjangkau.</p>
-          <button className="flex justify-center items-center w-[200px] h-[50px] my-10 shadow-lg transition-all duration-100 hover:border-2 bg-orange-500 ">
+          <button className="flex justify-center items-center w-[200px] h-[50px] my-10 shadow-lg transition-all duration-100 rounded hover:border-2 bg-orange-500 ">
             <a className="text-white" hyref="#home">
               Mau liat dong
             </a>
--- src/pages/MainPages/PaketProgram.jsx
+++ src/pages/MainPages/PaketProgram.jsx
@@ -3,24 +3,24 @@ import React from 'react';
 const PaketProgram = () => {
   return (
     <>
-      <div className="flex w-[50%] h-auto justify-between items-center text-white ">
-        <div className="flex flex-column h-[350px] w-[250px] bg-red-500 shadow-lg ">
+      <div className="flex w-[50%] h-auto justify-between items-center text-white  ">
+        <div className="flex flex-column h-[350px] w-[250px] bg-orange-800 rounded shadow-lg gap-5 items-center justify-center">
           <h1>Paket Umum</h1>
           <h3>Sisi </h3>
           <h5>Biaya</h5>
-          <button>Pilih Program</button>
+          <button className=" py-2 px-4 transition-all  rounded duration-200 hover:bg-gradient-to-r from-blue-600 to-blue-950">Pilih Program</button>
         </div>
-        <div className="flex flex-column h-[350px] w-[250px] bg-red-500 shadow-lg ">
+        <div className="flex flex-column h-[350px] w-[250px] bg-orange-800 rounded shadow-lg gap-5 items-center justify-center ">
           <h1>Paket Umum</h1>
           <h3>Sisi </h3>
           <h5>Biaya</h5>
-          <button>Pilih Program</button>
+          <button className=" py-2 px-4  transition-all rounded  duration-200 hover:bg-gradient-to-r from-blue-600 to-blue-950">Pilih Program</button>
         </div>
-        <div className="flex flex-column h-[350px] w-[250px] bg-red-500 shadow-lg ">
+        <div className="flex flex-column h-[350px] w-[250px] bg-orange-800 rounded shadow-lg gap-5 items-center justify-center ">
           <h1>Paket Umum</h1>
           <h3>Sisi </h3>
           <h5>Biaya</h5>
-          <button>Pilih Program</button>
+          <button className=" py-2 px-4  transition-all rounded  duration-200 hover:bg-gradient-to-r from-blue-600 to-blue-950">Pilih Program</button>
         </div>
       </div>
     </>

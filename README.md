d1f27108d2867243da70c3925d0e5bf7ee0bedd7
hardimahendra <hardimahendra35@gmail.com>
2024-02-23 10:50:37 +0700

    update

--- README.md
+++
@@ -1,1 +0,0 @@
-# lathy-private-frontend
--- src/pages/MainPages/Register.jsx
+++ src/pages/MainPages/Register.jsx
@@ -38,17 +38,31 @@ const Users = () => {
     <div className="flex justify-center items-center h-[100vh] w-full my-5xl text-white">
       <div className="relative flex flex-column justify-center items-center text-center h-[500px] w-[500px] shadow-lg rounded-lg">
         <div className="absolute flex justify-center items-center top-2 left-3 h-[50px] w[50px]">
-          <Link className="flex justify-center items-center rounded-circle p-3  h-full w-full border-none  hover:bg-orange-500 hover:text-blue-300 transition-all duration-200 " to="/home">
+          <Link className="flex justify-center items-center rounded-circle p-3  h-full w-full border-none  hover:bg-orange-500 hover:text-blue-300 transition-all duration-200 " to="/">
             <FaArrowLeft />
           </Link>
         </div>
-        <h4 className="text-lg font-bold py-4">Registrasi User</h4>
+        <h4 className="text-lg font-bold py-4">Daftar Menjadi Pelajar</h4>
         <Toaster position="top-center" />
         <form onSubmit={addUser}>
           <div className="flex flex-column gap-3">
             <input className=" w-[300px] h-[40px] rounded-lg text-black px-2" value={username} onChange={(e) => setUsername(e.target.value)} type="text" name="username" placeholder="Nama Lengkap..." required />
-            <input className=" w-[300px] h-[40px] rounded-lg text-black px-2" value={email} onChange={(e) => setEmail(e.target.value)} type="email" name="email" placeholder="Email/WA/Nomor HP" required />
-            <input className=" w-[300px] h-[40px] rounded-lg text-black px-2" value={password} onChange={(e) => setPassword(e.target.value)} type="password" name="passGuru" autoComplete="on" placeholder="Password" required />
+            <select className="text-black">
+              <option value="Jenjang">Jenjang</option>
+              <option value="SD">SD</option>
+              <option value="SMP">SMP</option>
+              <option value="SMA">SMA</option>
+              <option value="Kuliah">Kuliah</option>
+            </select>
+            <select className="text-black">
+              <option value="Paket Umum">Paket Umum</option>
+              <option value="Paket Khusus">Paket Khusus</option>
+              <option value="Paket Intensif">Paket Intensif</option>
+            </select>
+            <select className="text-black">
+              <option value="Online">Online</option>
+              <option value="Tatap Muka">Tatap Muka</option>
+            </select>
           </div>
           <div className="flex justify-center items-center py-4 ">
             <button type="submit" className="rounded-lg p-2  w-1/2 border-none  hover:bg-orange-500 hover:text-blue-300 transition-all duration-200 ">

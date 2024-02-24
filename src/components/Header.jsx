d39ec8e065ac7f037b51d50cfd3442486ac9ff27
hardimahendra <hardimahendra35@gmail.com>
2024-02-24 21:31:15 +0700

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,11 +8,11 @@ const Header = () => {
         <div className="relative flex flex-column justify-center items-left w-[50%] h-full">
           <h1 className="text-4xl text-orange-500 font-serif indent-12">Anak Jadi lebih rajin dan produktif dengan bimbingan belajar bersama kami di Lathy Private!</h1>
           <p className="text-xl text-gray-400 font-serif">Les Private dengan metode belajar interaktif, tutor Profesional dan investasi belajar yang murah dan terpercaya.</p>
-          <button className="absolute right-0 bottom-[20%] flex justify-center items-center md:w-[200px] md:h-[50px]  shadow-lg transition-all duration-100 rounded hover:border-2 bg-orange-500 ">
-            <a className="text-white " hyref="#home">
-              Mau liat dong
-            </a>
-          </button>
+          <div className="text-white">
+            <button className="absolute right-0 bottom-[20%] flex justify-center items-center md:w-[200px] md:h-[50px] shadow-lg transition-all duration-100 rounded bg-orange-600 hover:bg-transparent hover:border-2 hover:border-orange-500 hover:text-orange-500">
+              <a hyref="#home">Mau liat dong</a>
+            </button>
+          </div>
         </div>
         <div className="">
           <Register />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,7 @@ const index = () => {
         <div className="px-3 mx-auto flex justify-between items-center">
           {/* Large size */}
           <ul className="lg:flex gap-12 items-center text-lg hidden">
-            <NavLink className="flex justify-center items-center gap-2 text-yellow-500 font-semibold" to="/">
+            <NavLink className="flex justify-center items-center gap-2 text-orange-500 font-semibold" to="/">
               <img className="w-10" src="./icons/maskot.png" alt="brand" />
               <h1>Lathy Private</h1>
             </NavLink>

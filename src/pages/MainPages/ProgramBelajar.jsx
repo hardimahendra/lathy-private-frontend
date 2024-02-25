583fec0fa5e1c79de735aaab74393b15ad127452
hardimahendra <hardimahendra35@gmail.com>
2024-02-25 22:39:58 +0700

    update

--- src/pages/MainPages/ProgramBelajar.jsx
+++ src/pages/MainPages/ProgramBelajar.jsx
@@ -8,20 +8,20 @@ const ProgramBelajar = () => {
         <div className="flex text-white justify-center items-center w-[50%]">
           <div className="flex flex-column items-center justify-center">
             <h1 className="text-lg font-bold">Program Les Private</h1>
-            <div className="flex flex-row flex-wrap  md:justify-between justify-around items-center gap-2 w-full  my-4 p-2">
-              <button className="flex justify-around items-center gap-4 border-b-2 hover:border-orange-500 transition-all duration-100 w-[150px] h-[40px]" onClick={() => setIndex(0)}>
+            <div className="flex flex-row flex-wrap  sm:justify-around justify-center items-center gap-2 w-full  my-4 p-2">
+              <button className="flex justify-around items-center gap-4 border-b-2 hover:border-orange-500 hover:text-orange-500 transition-all duration-75 w-[150px] h-[40px]" onClick={() => setIndex(0)}>
                 SD
               </button>
-              <button className="flex justify-around items-center gap-4 border-b-2 hover:border-orange-500 transition-all duration-100 w-[150px] h-[40px] " onClick={() => setIndex(1)}>
+              <button className="flex justify-around items-center gap-4 border-b-2 hover:border-orange-500 hover:text-orange-500 transition-all duration-75 w-[150px] h-[40px] " onClick={() => setIndex(1)}>
                 SMP
               </button>
-              <button className="flex justify-around items-center gap-4 border-b-2 hover:border-orange-500 transition-all duration-100 w-[150px] h-[40px] " onClick={() => setIndex(2)}>
+              <button className="flex justify-around items-center gap-4 border-b-2 hover:border-orange-500 hover:text-orange-500 transition-all duration-75 w-[150px] h-[40px] " onClick={() => setIndex(2)}>
                 SMA
               </button>
-              <button className="flex justify-around items-center gap-4 border-b-2 hover:border-orange-500 transition-all duration-100 w-[150px] h-[40px] " onClick={() => setIndex(3)}>
+              <button className="flex justify-around items-center gap-4 border-b-2 hover:border-orange-500 hover:text-orange-500 transition-all duration-75 w-[150px] h-[40px] " onClick={() => setIndex(3)}>
                 Kuliah
               </button>
-              <button className="flex justify-around items-center gap-4 border-b-2 hover:border-orange-500 transition-all duration-100 w-[150px] h-[40px] " onClick={() => setIndex(3)}>
+              <button className="flex justify-around items-center gap-4 border-b-2 hover:border-orange-500 hover:text-orange-500 transition-all duration-75 w-[150px] h-[40px] " onClick={() => setIndex(4)}>
                 Kursus Khusus
               </button>
             </div>
@@ -111,6 +111,32 @@ const ProgramBelajar = () => {
             </div>
             <div className="flex flex-column text-left pt-2 gap-2 " hidden={index != 3}>
               <h1 className="font-semibold pb-2">Jenjang Diploma/Sarjana</h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero deleniti, quasi nam laborum ipsa quae incidunt blanditiis facere consequuntur veritatis, mollitia perspiciatis asperiores omnis architecto nulla, voluptatibus
+                aperiam quod eligendi earum aspernatur! Placeat, voluptas distinctio voluptatem commodi quas porro rem ullam soluta. Illo tempora saepe deserunt laudantium dignissimos sit repellendus, facere eius unde eveniet tenetur
+                consectetur beatae, eligendi porro quod repellat blanditiis nemo ipsa voluptatum assumenda totam ducimus quidem quia. Illum, placeat animi dolore cumque perferendis labore, nihil reprehenderit vel unde, omnis assumenda. Eius
+                est excepturi, cumque neque temporibus reiciendis distinctio at magnam unde ut vitae consequuntur inventore totam fugiat!
+              </p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ipsa? Ullam accusantium vel provident ducimus, laborum doloremque hic repudiandae in praesentium porro eos ex ut nam vitae omnis, voluptates tempore.</p>
+              <div className="flex flex-row pl-4  gap-5">
+                <ul className="list-disc">
+                  <li>Pengetahuan Kuantitatif</li>
+                  <li>Kemampuan Penalaran Umum</li>
+                  <li>Pengetahuan dan Pemahaman Umum</li>
+                  <li>Kemampuan Memahami bacaan dan menulis</li>
+                  <li>Saintek</li>
+                </ul>
+                <ul className="list-disc">
+                  <li>Soshum</li>
+                  <li>Literasi Bahasa Indonesia</li>
+                  <li>Literasi Bahasa Inggris</li>
+                  <li>SIMAK UI</li>
+                  <li>UTUL UGM</li>
+                </ul>
+              </div>
+            </div>
+            <div className="flex flex-column text-left pt-2 gap-2 " hidden={index != 4}>
+              <h1 className="font-semibold pb-2">Kursus Khusus</h1>
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero deleniti, quasi nam laborum ipsa quae incidunt blanditiis facere consequuntur veritatis, mollitia perspiciatis asperiores omnis architecto nulla, voluptatibus
                 aperiam quod eligendi earum aspernatur! Placeat, voluptas distinctio voluptatem commodi quas porro rem ullam soluta. Illo tempora saepe deserunt laudantium dignissimos sit repellendus, facere eius unde eveniet tenetur

2f50deac22dc6ae7a8a0eb389bd4e2797e260416
hardimahendra <hardimahendra35@gmail.com>
2024-02-29 14:52:21 +0700

    update

--- src/pages/MainPages/ProgramBelajar.jsx
+++ src/pages/MainPages/ProgramBelajar.jsx
@@ -6,7 +6,7 @@ const ProgramBelajar = () => {
   return (
     <>
       <div className="flex justify-center items-center w-full h-full py-4">
-        <div className="flex text-white justify-center items-center w-[50%]">
+        <div className="flex text-white justify-center items-center lg:w-[50%] w-[70%]">
           <div className="flex flex-column items-center justify-center">
             <h1 className="text-lg font-bold">Program Les Private</h1>
             <div className="flex flex-row flex-wrap  sm:justify-around justify-center items-center gap-2 w-full  my-4 p-2">

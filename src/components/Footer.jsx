fb04896777960c4be06530285ef017f11e37e2a8
hardimahendra <hardimahendra35@gmail.com>
2024-02-29 15:29:26 +0700

    Fixing Process Developer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const index = () => {
   return (
-    <div className=" flex flex-column justify-center items-center p-2 text-white">
+    <div className=" flex flex-column justify-center items-center p-2 text-white text-center">
       <p className="text-[12px]">CopyRight Reserved @2024. Lathy Private | Tempat Les Terdekat dan Murah</p>
       <p className="text-[12px]">
         Education Zone | Develop By{' '}
